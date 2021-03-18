bc2b2ee092d2c83c01e3134bbf972b307600f3c1
shenriquess <shenriquess@outlook.com>
2021-03-18 20:39:46 -0300

    ok

--- sloth.ts
+++ sloth.ts
@@ -4,8 +4,8 @@
 namespace sloth {
 
     export enum PWMChn {
-        Right_Leg = 0,
-        Right_Foot = 0,
+        Right_Leg = 6,
+        Right_Foot = 7,
         Left_Foot = 8,
         Left_Leg = 9,
         CH1 = 0,
